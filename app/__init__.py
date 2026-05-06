f1f78dfb00fa868da95d05c146b20343647fec10
Erol Yuksel <eroltheyksl@gmail.com>
2026-05-06 20:43:29 +0300

    fix: configure Flask static folder to root /static directory

--- app/__init__.py
+++ app/__init__.py
@@ -5,7 +5,7 @@ from app import database
 
 
 def create_app():
-    app = Flask(__name__, instance_relative_config=True)
+    app = Flask(__name__, instance_relative_config=True, static_folder="../static", static_url_path="/static")
 
     app.config.from_mapping(
         SECRET_KEY=os.environ.get("SECRET_KEY", "dev-secret-key"),
